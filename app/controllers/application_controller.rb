e5bc4d1a5ac78783ba3079f614a4fec0510c17f0
Alex Ricciardi <alex@thearrayschool.com>
2018-06-18 16:38:32 -0600

    onfigure_permitted_parameters for adims and accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:customer_number, :title, :first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:update, keys: [:customer_number, :title, :first_name, :last_name, :email])
+  end
 end
