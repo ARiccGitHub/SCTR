ceb6eccb1dfc3dbfd159b9d996fbcde90af65478
Alex Ricciardi <alex@thearrayschool.com>
2018-06-18 16:33:29 -0600

    Install devise to manage to login tables, admins and accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  ## Home page
+  root "webpages#index"
+
+  ## Devise routes modifed to match generated folder with scope
+     # Admin
+   devise_for :admins, path: 'admins', controllers: {
+     sessions: "admins/sessions",
+     passwords: "admins/passwords",
+     registrations: "admins/registrations",
+     confirmations: "admins/confirmations"
+   }
+   # Account
+   devise_for :accounts, path: 'contacts', controllers: {
+     sessions: "accounts/sessions",
+     passwords: "accounts/passwords",
+     registrations: "accounts/registrations",
+     confirmations: "accounts/confirmations"
+   }
 end
